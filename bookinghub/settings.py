69bf5523dd92c636b1091b0adf181ce793a85a7d
Matteo <m.cafarotti@gmail.com>
2024-11-12 21:59:33 +0100

    implemented postgresql integration

--- bookinghub/settings.py
+++ bookinghub/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'bookinghub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'mydatabase'),
+        'USER': os.getenv('POSTGRES_USER', 'myuser'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'mypassword'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
